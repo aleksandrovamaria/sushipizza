1f10f1a492af7b21c6bdcd9a5fdea47279daa6d4
aleksandrova maria <m.aleksandrova287@yandex.ru>
2022-08-24 11:00:40 +0300

    implement saving district for session

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,13 @@
 package config
 
-import "html/template"
+import (
+	"github.com/alexedwards/scs/v2"
+	"html/template"
+)
 
 type AppConfig struct {
 	InProduction  bool
 	UseCache      bool
 	TemplateCache map[string]*template.Template
+	Session       *scs.SessionManager
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"strconv"
 	"sushipizza/internal/config"
 	"sushipizza/internal/models"
 	"sushipizza/internal/render"
@@ -28,8 +29,14 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 		{1, "Мурино", time.Now(), time.Now()},
 		{2, "Колтуши", time.Now(), time.Now()},
 	}
+	if m.App.Session.Get(r.Context(), "current-district") == nil {
+		m.App.Session.Put(r.Context(), "current-district", districts[0])
+	}
+
 	data := make(map[string]interface{})
 	data["districts"] = districts
+	data["current-district"] = m.App.Session.Get(r.Context(), "current-district")
+
 	err := render.Template(w, r, "home.page.tmpl", &models.TemplateData{
 		Data: data,
 	})
@@ -39,15 +46,53 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	err := render.Template(w, r, "about.page.tmpl", &models.TemplateData{})
+	districts := []models.District{
+		{1, "Мурино", time.Now(), time.Now()},
+		{2, "Колтуши", time.Now(), time.Now()},
+	}
+	if m.App.Session.Get(r.Context(), "current-district") == nil {
+		m.App.Session.Put(r.Context(), "current-district", districts[0])
+	}
+
+	data := make(map[string]interface{})
+	data["districts"] = districts
+	data["current-district"] = m.App.Session.Get(r.Context(), "current-district")
+
+	err := render.Template(w, r, "about.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
 	if err != nil {
 		log.Println(err)
 	}
 }
 
 func (m *Repository) Districts(w http.ResponseWriter, r *http.Request) {
-	err := render.Template(w, r, "districts.page.tmpl", &models.TemplateData{})
+	districts := []models.District{
+		{1, "Мурино", time.Now(), time.Now()},
+		{2, "Колтуши", time.Now(), time.Now()},
+	}
+	if m.App.Session.Get(r.Context(), "current-district") == nil {
+		m.App.Session.Put(r.Context(), "current-district", districts[0])
+	}
+
+	data := make(map[string]interface{})
+	data["districts"] = districts
+	data["current-district"] = m.App.Session.Get(r.Context(), "current-district")
+
+	err := render.Template(w, r, "districts.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func (m *Repository) ChangeDistrict(w http.ResponseWriter, r *http.Request) {
+	m.App.Session.Remove(r.Context(), "current-district")
+	id, _ := strconv.Atoi(r.URL.Query()["id"][0])
+	name := r.URL.Query()["name"][0]
+
+	m.App.Session.Put(r.Context(), "current-district", models.District{ID: id, Name: name})
+
+	http.Redirect(w, r, r.Header.Get("Referer"), 302)
 }
